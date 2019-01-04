0692a31f7a2dbd8794e84be0b56c303136ac5be1
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2019-01-04 13:08:20 +0100

    used webpack plugins to add file index.html in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require("path"); //includiamo questo comando node per avere l'indirizzo assoluto
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 //comando nodejs per esportare oggetto contentente configurazioni
 module.exports = {
   entry: "./src/js/index.js", //dove iniziare il bundle (1 o più files), quello che deve eseguire
   output: {
     //specifichiamo dove salvare il bundle file risultato, con indirizzo e nome file
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js"
-  }
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  devServer: {
+    //configurazione di webpack-dev-server
+    contentBase: "./dist" //cartella da servire
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ]
 };
