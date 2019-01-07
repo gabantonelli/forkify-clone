cde1229202ac34456b554f9ad3fa1ce942858d07
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2019-01-07 14:00:04 +0100

    created the Search model and tested in index.js

---
+++ src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import * as config from "../APIconfig";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    try {
+      const res = await axios(
+        `${config.proxy}https://www.food2fork.com/api/search?key=${
+          config.key
+        }&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
