a04e5c5a1900478c7e47908c257cc83667e0faad
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2019-01-10 08:30:42 +0100

    added functionality to read query from the search form

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,32 @@
 // // import * as searchView from './views/searchView' e poi richiamarli con searchView.a, searchView.ID etc...
 // console.log(multiply(ID, 2));
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+/** GLOBAL STATE OF THE APP in other complex apps done by Redux
+ * - Search object with all data about the search query and results
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  //1. get query from the view
+  const query = searchView.getInput();
+  if (query) {
+    //2. new search object and add it to state
+    state.search = new Search(query);
+    //3. prepare for displaying results on UI
+    //4. search for recipes
+    await state.search.getResults();
+    //5. render results on UI
+    console.log(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault(); //to stop the page from reloading when submitting
+  controlSearch();
+});
